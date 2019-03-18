e41d990d61838c3f759d68d1cc8712beec1328a0
ZENBOOK\fimab <yafimb@zoara.com>
2019-03-18 11:38:41 +0200

    Modifying products details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -22,8 +22,13 @@ class ProductResource extends JsonResource
             'name'          => $this->name,
             'description'   => $this->detail,
             'price'         => $this->price,
-            'stock'         => $this->stock,
+            'stock'         => $this->stock != 0 ? $this->stock : 'Out of stock',
             'discount'      => $this->discount,
+            'totsl price'   => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating'        => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No rating yet',
+            'href'=>[
+                'reviews'=>route('reviews.index', $this->id),
+            ],
         ];
     }
 }
