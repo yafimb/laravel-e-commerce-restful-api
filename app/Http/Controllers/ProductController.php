e8869bbf0b44ae488e7caa6c7d48015ca67ecdad
ZENBOOK\fimab <yafimb@zoara.com>
2019-03-20 15:35:51 +0200

    Product delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,14 +69,10 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param Product $product
      */
     public function edit(Product $product)
     {
-        echo"1";
     }
 
     /**
@@ -97,13 +93,11 @@ class ProductController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Product  $product
-     * @return \Illuminate\Http\Response
+     * @param Product $product
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return \response(['data' => null], Response::HTTP_NO_CONTENT);
     }
 }
